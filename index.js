175616490ae87c4f9c2276fa1809931d99f0e9e5
Samit <devilgamingbrawlstars@gmail.com>
2024-06-10 09:06:43 +0545

    Changes

--- index.js
+++ index.js
@@ -3,21 +3,9 @@ const { readFile } = require('fs');
 const { createCanvas } = require('canvas');
 const mysql = require('mysql');
 const QRCode = require('qrcode');
+const path = require('path');
 const app = express();
 const canvas = createCanvas(250, 250);
-
-app.use(express.static(__dirname));
-
-//Starting sever
-app.get('/', (req, res) => {
-    readFile('./body.html', 'utf8', (err, html) => {
-        if (err) {
-            res.send(err);
-        }
-        res.send(html);
-    });
-});
-//Creating connection with mySQL Database
 const con = mysql.createConnection({
     host: "localhost",
     user: "samit",
@@ -25,35 +13,45 @@ const con = mysql.createConnection({
     database: 'mydb'
 });
 
-con.connect(function(err) {
-    if (err) throw err;
+con.connect(err => {
+    if (err) {
+        console.error("Error connecting to MySQL database:", err);
+        process.exit(1);
+    }
     console.log("Connected to MySQL database!");
 });
-//Function to generate base64 encoded qrcode image.
-async function createQR(url){
-    let canvas = createCanvas(250, 250);
-    QRCode.toCanvas(canvas, url, function(err){
-        if(err){
-            console.log('Error generating QRCode');
+
+async function createQR(url) {
+    try {
+        let canvas = createCanvas(250, 250);
+        await QRCode.toCanvas(canvas, url);
+        console.log('QRCode created');
+        const base64Image = canvas.toDataURL().split(',')[1];
+        return base64Image;
+    } catch (err) {
+        console.error('Error generating QRCode:', err);
+        return null;
+    }
+}
+
+function updateDB(QrCodeImg, Hotel_id) {
+    con.query('UPDATE hotel SET QRCode = ? WHERE Hotel_id = ?', [QrCodeImg, Hotel_id], (err, result) => {
+        if (err) {
+            console.error(`Error updating QRCode for Hotel_id ${Hotel_id}:`, err);
             return;
         }
-        console.log('QRCode created');
+        console.log(`QRCode updated for Hotel_id: ${Hotel_id}`);
     });
-    const base64Image = canvas.toDataURL().split(',')[1];
-    return base64Image;
 }
-function updateDB(QrCodeImg,Hotel_id){
-    con.query('UPDATE hotels SET QRCode = ? WHERE Hotel_id = ?', [QrCodeImg, Hotel_id], (err, result)=>{
-        if(err) throw err;
-        console.log(`Qrcode Updated for Hotel_id: ${Hotel_id}`);
-    })
-}
-//Function that checks into db if any of the rows have null value in QRCode field. 
+
 async function CheckDB() {
-    con.query('SELECT * FROM Hotels WHERE QRCode IS NULL', async (err, result) => {
-        if (err) throw err;
+    con.query('SELECT * FROM hotel WHERE QRCode IS NULL', async (err, result) => {
+        if (err) {
+            console.error('Error querying database:', err);
+            return;
+        }
         for (const row of result) {
-            const url = `http://localhost:3000/body.html/${row.Hotel_id}`;
+            const url = `http://localhost:3000/hotel/${row.Hotel_id}`;
             const QrCodeImg = await createQR(url);
             if (QrCodeImg) {
                 updateDB(QrCodeImg, row.Hotel_id);
@@ -61,13 +59,50 @@ async function CheckDB() {
         }
     });
 }
-const interval= setInterval(CheckDB, 500);
-process.on('SIGINT', function() {
+
+app.get('/', (req, res) => {
+    app.use(express.static(__dirname));
+    res.send('Welcome to the Hotel QR Code Service!');
+});
+
+app.get('/hotel/:Hotel_id', (req, res) => {
+    app.use(express.static(__dirname));
+    const hotelId = req.params.Hotel_id;
+    con.query('SELECT * FROM hotel WHERE Hotel_id = ?', [hotelId], (err, result) => {
+        if (err) {
+            console.error('Error fetching hotel data:', err);
+            res.status(500).send('Internal server error');
+            return;
+        }
+        if (result.length === 0) {
+            res.status(404).send('No hotel found.');
+            return;
+        }
+        const hotel = result[0];
+        readFile(path.join(__dirname, 'body.html'), 'utf8', (err, html) => {
+            if (err) {
+                console.error('Error reading HTML file:', err);
+                res.status(500).send('Internal server error');
+                return;
+            }
+            html = html.replace('{{name}}', hotel.Hotel_name);
+            html = html.replace('{{hotelId}}',hotelId);
+            res.send(html);
+        });
+    });
+});
+
+process.on('SIGINT', () => {
     clearInterval(interval);
-    con.end(function(err) {
-        if (err) throw err;
-        console.log('MySQL connection closed');
+    con.end(err => {
+        if (err) {
+            console.error('Error closing MySQL connection:', err);
+        } else {
+            console.log('MySQL connection closed');
+        }
         process.exit();
     });
 });
-app.listen(process.env.PORT || 3000, () => console.log('Web available on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Web available on http://localhost:${PORT}`));
+const interval = setInterval(CheckDB, 5000);
